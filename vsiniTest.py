1296e6c9ee03c7e21535f2e56e9632ddade345d7
jgussman <jgussman@iu.edu>
2020-02-16 15:42:05 -0500

    LineBroadening function made

--- vsiniTest.py
+++ vsiniTest.py
@@ -3,13 +3,59 @@ import matplotlib.pylab as plt
 from PyAstronomy import pyasl
 
 
-# Create data with a Gaussian absoprtion line
 
-wvl,flux = np.loadtxt("C:\\Users\\pirat\\Research\\SOSA\\Testing\\6700.txt",unpack= True)
+##Create data with a Gaussian absoprtion line
+
+wvl,flux = np.loadtxt("C:\\Users\\pirat\\Research\\SOSA\\SS\\6700.txt",unpack= True)
 bwvl, bflux = np.loadtxt("C:\\Users\\pirat\\Research\\SOSA\\Binaries\\HIP109303\\109303a16.txt",unpack = True)
 
-# Apply the fast algorithm and ...
-bfast = pyasl.fastRotBroad(wvl, flux, 0.0, 50)
+# # Apply the fast algorithm and ...
+# bfast = pyasl.fastRotBroad(wvl, flux, 0.0, 50)
+
+
+
+def MakingCombinations(LeftSS,RightSS,wav_binary,flux_binary,delta_weight):
+    '''
+    Input: {(Left SS Temp,L Epsilon,L vsini) : [wavelength,flux]}, {(Right SS Temp,R Epsilon, R vsini) : [wavelength,flux]}, {wavelengths of binary : flux of binary}, incredment for the weight 
+    Output: {(Left SS Temp, L Weight, L Epsilon, L vsini, Right SS Temp, R Weight, R Epsilon, R vsini) : Standard deviation from the observed spectra}
+    '''
+    combinations={}
+    weightrange = np.arange(delta_weight,100,delta_weight)
+    for LeftStarDetails in LeftSS:
+        leftTemp = LeftStarDetails[0]
+        leftEpsilon = LeftStarDetails[1]
+        leftVsini = LeftStarDetails[2]
+        for RightStarDetails in RightSS:
+            rightTemp = RightStarDetails[0]
+            rightEpsilon = RightStarDetails[1]
+            rightVsini = RightStarDetails[2]
+            wav_sum=(LeftSS[leftTemp][0]+RightSS[rightTemp][0])/2
+            indexINTOsum=np.where(np.isin(wav_sum,wav_binary))
+            indexINTObinary=np.where(np.isin(wav_binary,wav_sum)) 
+            for weight in weightrange:
+                weight=float(weight)
+                leftWeighted=LeftSS[leftTemp][1]*weight/100.
+                rightWeighted=RightSS[rightTemp][1]*(100.-weight)/100.
+                flux_sum=(leftWeighted + rightWeighted)/2
+                combinations[((leftTemp,weight/100.,leftEpsilon,leftVsini,rightTemp,(100.-weight)/100.,rightEpsilon,rightVsini))]=(flux_sum[indexINTOsum]/flux_binary[indexINTObinary]).std()  
+    return combinations 
+
+#Where to put the vsini in SOSA: 1) Inside the possible combinations function
+#All the SS need to be broadened before the go into possible combinations 
+
+def LineBroadening(SSwav,SSflux,epsilon,vsini):
+    '''
+    Input: Synethic Spectra's wavelength (regularly spaced,numpy array), Synethic Spectra's flux (numpy array), epsilon(float), vsini(float)
+    Output: Inputed wavelength, Line Broadened flux (Both numpy array) 
+    Using:  pyasl.fastRotBroad from the PyAstronomy package 
+    '''
+    return SSwav, pyasl.fastRotBroad(SSwav, SSflux , epsilon , vsini)
+ 
+ 
+
+wvl, bfast = LineBroadening(wvl,flux,0.2,100.)
+
+
 
 
 plt.xlabel("Wvl [A]")
@@ -20,9 +66,4 @@ plt.plot(wvl, bfast, 'b.-')
 plt.show()
 
 
-def LineBroadening(SSwav,SSflux):
-    '''
-    Input: Synethic Spectra's wavelength (Make sure they are evenly spaced,numpy array), Synethic Spectra's flux (numpy array)
-    Output: Line Broadened wav, flux (Both numpy array) 
-    '''
-    
+
